b3a87829b65f964afc4ce142d4ea489a3fa554be
Diego Ongaro <ongardie@gmail.com>
2020-08-20 13:58:06 -0700

    Enable syntax higlighting for markdown code blocks

--- template.py
+++ template.py
@@ -3,11 +3,20 @@
 
 from mako.template import Template
 from markdown import markdown
+from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.codehilite import CodeHiliteExtension
 
 
 def render_file(file, args):
     body = open(file).read()
     if str(file).endswith('.md'):
-        body = markdown(body, extensions=['fenced_code'])
+        body = markdown(body, extensions=[
+            CodeHiliteExtension(
+                guess_lang=False,
+                linenums=False,
+                noclasses=True,
+            ),
+            FencedCodeExtension(),
+        ])
     out = Template(body, strict_undefined=True)
     return out.render(**args)
