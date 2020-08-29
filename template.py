27d9a8e5a0154aa7597104a2bd70a5fe1041f6da
Diego Ongaro <ongardie@gmail.com>
2020-08-28 17:43:33 -0700

    Fix Markdown processing: run Mako first
    This ensures that Markdown doesn't emit anything that's interpreted by Mako and
    that the raw input files can use Mako to escape things or emit things that
    Markdown can then read.

--- template.py
+++ template.py
@@ -5,12 +5,30 @@ from mako.template import Template
 from markdown import markdown
 from markdown.extensions.fenced_code import FencedCodeExtension
 from markdown.extensions.codehilite import CodeHiliteExtension
+import re
+
+
+# A line starting with a '##' is a comment in Mako and a header in Markdown.
+# We want Markdown's meaning,
+def md_preprocessor(input):
+    output = []
+    for line in input.split('\n'):
+        m = re.match(r'^[\t ]*(#{2,6}) (.*)$', line)
+        if m is None:
+            output.append(line)
+        else:
+            size = str(len(m.group(1)))
+            output.append(f'<h{size}>{m.group(2)}</h{size}>')
+    return '\n'.join(output)
 
 
 def render_file(file, args):
     body = open(file).read()
     if str(file).endswith('.md'):
-        body = markdown(body, extensions=[
+        out = Template(body,
+                       preprocessor=md_preprocessor,
+                       strict_undefined=True).render(**args)
+        return markdown(out, extensions=[
             CodeHiliteExtension(
                 guess_lang=False,
                 linenums=False,
@@ -18,5 +36,5 @@ def render_file(file, args):
             ),
             FencedCodeExtension(),
         ])
-    out = Template(body, strict_undefined=True)
-    return out.render(**args)
+    else:
+        return Template(body, strict_undefined=True).render(**args)
